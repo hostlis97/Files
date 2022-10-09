d71ca1705f9f6d17175fc13a28b985af744718ed
HostLis <hostlis@tmpk.net>
2022-10-10 00:49:03 +0300

    Добавление функционала сохранения/загрузки корзины

--- src/Basket.java
+++ src/Basket.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class Basket {
     public int price[];
     public String products[];
@@ -10,7 +12,7 @@ public class Basket {
         cart = new int[products.length];
     }
 
-    public void cart(){
+    public void cart() {
         System.out.println("Ваша корзина:");
         for (int i = 0; i < cart.length; i++) {
             if (cart[i] != 0) {
@@ -24,5 +26,35 @@ public class Basket {
         cart[productNumber] += productCount;
         total += productCount * price[productNumber];
     }
+
+    protected void saveTxt(File file) throws IOException {
+        try (FileWriter writer = new FileWriter(file.getName(), false)) {
+            for (int i = 0; i < cart.length; i++) {
+                if (cart[i] != 0) {
+                    writer.write(cart[i] + " шт " + price[i] + " руб/шт " + cart[i] * price[i] + " руб в сумме\n");
+                }
+            }
+            writer.write("Итого: " + total + " руб");
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    protected static void loadFromBinFile(File file, Basket cart) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(file.getName()))) {
+            String s;
+            int product = 0;
+            while ((s = br.readLine()) != null) {
+                String parse[] = s.split(" ");
+                cart.addCart(Integer.parseInt(parse[0]), product);
+                product++;
+            }
+        } catch (IOException ex) {
+            ex.getMessage();
+        } catch (NumberFormatException ex) {
+        }
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
         String products[] = {"Хлеб", "Яблоки", "Молоко"};
         int price[] = {100, 200, 300};
         Basket cart = new Basket(price, products);
+        File file = new File("basket.txt");
+
+        if (file.exists()) {
+            System.out.println("Файл существует");
+            Basket.loadFromBinFile(file, cart);
+        } else {
+            System.out.println("Файл не существует");
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("Файл для сохранения корзины создан!");
+                } else {
+                    System.out.println("Файл для сохранения корзины не создан!");
+                }
+            } catch (IOException e) {
+                e.getMessage();
+            }
+        }
+
         for (int i = 0; i < products.length; i++) {
             System.out.println(i + 1 + ". " + products[i] + " " + price[i] + " руб/шт");
         }
@@ -25,6 +45,7 @@ public class Main {
                 int productNumber = Integer.parseInt(parts[0]) - 1;
                 int productCount = Integer.parseInt(parts[1]);
                 cart.addCart(productCount, productNumber);
+                cart.saveTxt(file);
                 if ((productNumber > products.length - 1) || (productNumber < 0)) {
                     System.out.println("Товара с таким номером нет в списке");
                     continue;
@@ -36,6 +57,8 @@ public class Main {
             } catch (NumberFormatException e) {
                 System.out.println("неправильный формат двнных");
                 continue;
+            } catch (IOException e) {
+                e.getMessage();
             }
         }
     }
